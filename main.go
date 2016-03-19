7ff7623b3f524c3e8405c053946cf6b5b04786e4
kendo5731 <aur.baumann@gmail.com>
2016-03-19 19:42:37 +0100

    feat(): adds support for -watch option
    Close #1

--- main.go
+++ main.go
@@ -10,10 +10,12 @@ import (
 
 	"github.com/aubm/postmanerator/postman"
 	"github.com/aubm/postmanerator/theme/helper"
+	"github.com/howeyc/fsnotify"
 )
 
 var theme = flag.String("theme", "markdown_default", "the theme to use")
 var outputFile = flag.String("output", "", "the output file, default is stdout")
+var watch = flag.Bool("watch", false, "automatically regenerate the output when the theme changes")
 
 var out *os.File = os.Stdout
 
@@ -38,9 +40,61 @@ func main() {
 
 	col.ExtractStructuresDefinition()
 
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("error: %v\n", r)
+		}
+	}()
+	generate(out, col)
+
+	if *watch {
+		watchDir(*theme, func() { generate(out, col) })
+	}
+}
+
+func generate(out *os.File, col *postman.Collection) {
+	fmt.Print("Generating output ... ")
+	out.Truncate(0)
 	templates := template.Must(template.New("").Funcs(helper.GetFuncMap()).ParseGlob(fmt.Sprintf("%v/index.tpl", *theme)))
-	err = templates.ExecuteTemplate(out, "index.tpl", *col)
+	err := templates.ExecuteTemplate(out, "index.tpl", *col)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
+	fmt.Print("done.\n")
+}
+
+func watchDir(dir string, action func()) {
+	watcher, err := fsnotify.NewWatcher()
 	checkErr(err)
+	defer watcher.Close()
+
+	done := make(chan bool)
+
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Printf("error: %v\n", r)
+			}
+			watchDir(dir, action)
+		}()
+
+		for {
+			select {
+			case ev := <-watcher.Event:
+				if !ev.IsAttrib() {
+					action()
+				}
+			case err := <-watcher.Error:
+				log.Printf("error: %v\n", err)
+			}
+		}
+	}()
+
+	err = watcher.Watch(dir)
+	checkErr(err)
+
+	<-done
 }
 
 func checkErr(err error) {
